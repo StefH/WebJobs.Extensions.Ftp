08c672aedf30840b1e3faa9ec420f8f228ecd685
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-09-23 10:37:30 +0200

    2

--- examples/Trigger.Sample.Ftp/HttpSample.cs
+++ examples/Trigger.Sample.Ftp/HttpSample.cs
@@ -1,11 +1,8 @@
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Trigger.Sample.Ftp;
 
@@ -19,25 +16,5 @@ public static class HttpSample
         string name = req.Query["name"];
 
         return new OkObjectResult($"Hello, {name}.");
-    }
-
-    [FunctionName("Function2")]
-    public static async Task<IActionResult> Run2(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
-    {
-        log.LogInformation("C# HTTP trigger function processed a request.");
-
-        string name = req.Query["name"];
-
-        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-        name = (name ?? data?.name) ?? "?";
-
-        string responseMessage = string.IsNullOrEmpty(name)
-            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-        return new OkObjectResult(responseMessage);
     }
 }
